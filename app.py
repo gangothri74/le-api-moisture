a5995ebd226ea370516b54cc052ae0e7a6c77198
gangothri74 <rachamantiganga@gmail.com>
2024-12-21 13:55:51 +0530

    updated

--- app.py
+++ app.py
@@ -14,19 +14,14 @@ def home():
 
 @api.route('/predict',methods=['GET'])
 def predict():
-    moisture_value = int(request.args.get('moisture_value'))
+    moisture_value = request.args.get('moisture_value', type=int)
 
-
-    if moisture_value is None:
+if moisture_value is None:
         return jsonify({'error': 'moisture_value is required'}), 400
 
-    
-    if moisture_value < MOISTURE_THRESHOLD:
-        return jsonify({'moisture_value': moisture_value, 'motor_status': 'ON'})
-    else:
-        return jsonify({'moisture_value': moisture_value, 'motor_status': 'OFF'})
-
-
+return jsonify({'moisture_value': moisture_value, 'motor_status': 'ON' if moisture_value < 50 else 'OFF'})
+response=le.predict(data)[0]
+return response
 if __name__=="__main__":
     api.run(
         host='0.0.0.0',
